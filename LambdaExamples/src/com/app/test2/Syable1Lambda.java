5b97ea55b9b0134fce2b665a3854849539944860
shaik <shaik@192.168.4.112>
2019-05-09 14:40:43 +0530

    Syable1 labda newly Addes

--- LambdaExamples/src/com/app/test2/Syable1Lambda.java
+++ LambdaExamples/src/com/app/test2/Syable1Lambda.java
@@ -3,7 +3,7 @@ package com.app.test2;
 public class Syable1Lambda {
 
 public static void main(String[] args) {
-	Sayable1 s1=new Sayable1() {
+/*	Sayable1 s1=new Sayable1() {
 		
 		@Override
 		public String say(String name) {
@@ -12,6 +12,10 @@ public static void main(String[] args) {
 		}
 	};
 	System.out.println(s1.say("jai"));
+	*/
 	
+	
+	Sayable1 s1=(name)->{return "Hello::"+name;};
+	System.out.println(s1.say("baja"));
 }
 }
