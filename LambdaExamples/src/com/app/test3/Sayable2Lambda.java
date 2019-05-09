6969ecf028f58f0270f48f68872ea8236c6a6995
shaik <shaik@192.168.4.112>
2019-05-09 14:44:11 +0530

    Sayable2 new return lambda Added

--- LambdaExamples/src/com/app/test3/Sayable2Lambda.java
+++ LambdaExamples/src/com/app/test3/Sayable2Lambda.java
@@ -13,9 +13,16 @@ public class Sayable2Lambda {
 		};
 		System.out.println(s2.add(5, 7));*/
 		
-		
+		//WithOut Return Key Word
 		Sayable2 s2=(a,b)->(a+b);
 		System.out.println(s2.add(40,50));
+		
+		//With return key Word
+		
+		Sayable2 s3=(a,b)->{
+			return a+b;
+		};
+		System.out.println("With retrn ::"+s2.add(10,50));
 	}
 	
 }
