31293a89378c1d3d08e335f732e304493bf919e4
shaik <shaik@192.168.4.112>
2019-05-09 14:27:19 +0530

    Added thye Saybl1 Second Commit

---
+++ LambdaExamples/src/com/app/test2/Syable1Lambda.java
@@ -0,0 +1,17 @@
+package com.app.test2;
+
+public class Syable1Lambda {
+
+public static void main(String[] args) {
+	Sayable1 s1=new Sayable1() {
+		
+		@Override
+		public String say(String name) {
+			// TODO Auto-generated method stub
+			return "Hiii Parameter String::"+name;
+		}
+	};
+	System.out.println(s1.say("jai"));
+	
+}
+}
